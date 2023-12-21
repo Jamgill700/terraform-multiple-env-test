17b62ce7e616b6860784b2ebbeb489942fa7df67
James Gillen <jamgill700@gmail.com>
2023-12-21 21:32:43 +0000

    gt

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 # Create a resource group
 resource "azurerm_resource_group" "multiple_env_test" {
-  name     = "multiple-env"
+  name     = "multiple-env-${var.environment}"
   location = var.location
 }
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "multiple_env_test" {
-  name                = "multiple-env-network"
-  resource_group_name = azurerm_resource_group.multiple_env.name
-  location            = azurerm_resource_group.multiple_env.location
+  name                = "multiple-env-${var.environment}-network"
+  resource_group_name = azurerm_resource_group.multiple_env_test.name
+  location            = azurerm_resource_group.multiple_env_test.location
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "multiple_env_test" {
-  name                 = "management-zone"
-  resource_group_name  = azurerm_resource_group.multiple_env.name
-  virtual_network_name = azurerm_virtual_network.multiple_env.name
+  name                 = "${var.environment}-management-zone"
+  resource_group_name  = azurerm_resource_group.multiple_env_test.name
+  virtual_network_name = azurerm_virtual_network.multiple_env_test.name
   address_prefixes     = ["10.0.1.0/24"]
 }
