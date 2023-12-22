09c899bf9f510de483277b24e461bee162528bf1
James Gillen <jamgill700@gmail.com>
2023-12-22 09:41:16 +0000

    3

--- tfc-workspace.tf
+++ tfc-workspace.tf
@@ -17,28 +17,28 @@ data "tfe_project" "tfc_project" {
 # to Azure with the permissions set in the Azure policy.
 #
 # https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/workspace
-resource "tfe_workspace" "my_workspace" {
-  name         = var.tfc_workspace_name
-  organization = var.tfc_organization_name
-  project_id   = data.tfe_project.tfc_project.id
-}
+# resource "tfe_workspace" "my_workspace" {
+#   name         = var.tfc_workspace_name
+#   organization = var.tfc_organization_name
+#   project_id   = data.tfe_project.tfc_project.id
+# }
 
 # The following variables must be set to allow runs
 # to authenticate to Azure.
 #
 # https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/variable
-resource "tfe_variable" "enable_azure_provider_auth" {
-  workspace_id = tfe_workspace.my_workspace.id
+# resource "tfe_variable" "enable_azure_provider_auth" {
+#   workspace_id = var.tfe_workspace_id
 
-  key      = "TFC_AZURE_PROVIDER_AUTH"
-  value    = "true"
-  category = "env"
+#   key      = "TFC_AZURE_PROVIDER_AUTH"
+#   value    = "true"
+#   category = "env"
 
-  description = "Enable the Workload Identity integration for Azure."
-}
+#   description = "Enable the Workload Identity integration for Azure."
+# }
 
 resource "tfe_variable" "tfc_azure_client_id" {
-  workspace_id = tfe_workspace.my_workspace.id
+  workspace_id = var.tfe_workspace_id
 
   key      = "TFC_AZURE_RUN_CLIENT_ID"
   value    = azuread_application.multiple_env.client_id
