1b6b2615a2705fc000f845533578ec1bdddc65f0
James Gillen <jamgill700@gmail.com>
2023-12-22 10:15:46 +0000

    11

--- global.auto.tfvars
+++ global.auto.tfvars
@@ -4,4 +4,3 @@ environment = "dev"
 tenant_id = "17624fa9-cd53-44aa-8a99-c8d2b713fc5b"
 tfc_workspace_name = "terraform-multiple-env-dev"
 oidc_client_id = "5a18a8ef-3037-46d1-9cc0-b324b8723f67" # jg-sp-001
-tfe_workspace_id = "ws-zPXS7JY5JjXrhArA"
--- variables.tf
+++ variables.tf
@@ -21,6 +21,3 @@ variable "tfc_project_name" {
 variable "tfc_workspace_name" {}
 variable "oidc_client_id" {}
 variable "oidc_client_secret" {}
-# variable "fed_cred_client_id" {}
-# variable "fed_cred_object_id" {}
-variable "tfe_workspace_id" {}
